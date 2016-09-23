986164c3c0fb622400624f7bc8fc9d625f8c2f09
Roger-Wu <gsetcrw@gmail.com>
2016-09-23 16:07:38 +0800

    q1 done

--- hw0/q1.py
+++ hw0/q1.py
@@ -1,5 +1,18 @@
+import sys
+import numpy as np
+
 def main():
-    pass
+    colIdx = int(sys.argv[1])
+    inFileName = sys.argv[2]
+    outFileName = 'ans1.txt'
+
+    mat = np.loadtxt(inFileName)
+    col = mat[:, colIdx]
+    col.sort()
+
+    with open(outFileName, 'w') as outFile:
+        print(','.join(map(str, col.tolist())), file=outFile)
+        # outFile.write(','.join(map(str, col.tolist())))
 
 if __name__ == '__main__':
     main()
